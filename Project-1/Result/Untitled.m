fd1c4b3852dca7a3c961ded022f370d009403f56
Furkan <tokgoz.furkan@metu.edu.tr>
2020-05-02 23:34:48 +0300

    Project 3 calculation

---
+++ Project-1/Result/Untitled.m
@@ -0,0 +1,58 @@
+k_scaling = 4;          % scaling factor of the figure
+% (You need to plot a figure which has a width of (8.8 * k_scaling)
+% in MATLAB, so that when you paste it into your paper, the width will be
+% scalled down to 8.8 cm  which can guarantee a preferred clearness.
+k_width_hight = 2;      % width:hight ratio of the figure
+
+width = 8.8 * k_scaling;
+hight = width / k_width_hight;
+
+%figure margins
+top = 0.5;  % normalized top margin
+bottom = 3;	% normalized bottom margin
+left = 3.5;	% normalized left margin
+right = 1;  % normalized right margin
+
+% set default figure configurations
+set(0,'defaultFigureUnits','centimeters');
+set(0,'defaultFigurePosition',[0 0 width hight]);
+
+set(0,'defaultLineLineWidth',1*k_scaling);
+set(0,'defaultAxesLineWidth',0.25*k_scaling);
+
+set(0,'defaultAxesGridLineStyle',':');
+set(0,'defaultAxesYGrid','on');
+set(0,'defaultAxesXGrid','on');
+
+set(0,'defaultAxesFontName','Times New Roman');
+set(0,'defaultAxesFontSize',8*k_scaling);
+
+set(0,'defaultTextFontName','Times New Roman');
+set(0,'defaultTextFontSize',8*k_scaling);
+
+set(0,'defaultLegendFontName','Times New Roman');
+set(0,'defaultLegendFontSize',8*k_scaling);
+
+set(0,'defaultAxesUnits','normalized');
+set(0,'defaultAxesPosition',[left/width bottom/hight (width-left-right)/width  (hight-bottom-top)/hight]);
+
+% set(0,'defaultAxesColorOrder',[0 0 0]);
+% set(0,'defaultAxesTickDir','out');
+
+set(0,'defaultFigurePaperPositionMode','auto');
+
+% you can change the Legend Location to whatever as you wish
+set(0,'defaultLegendLocation','northeast');
+set(0,'defaultLegendBox','on');
+set(0,'defaultLegendOrientation','vertical');
+hold all
+uiopen('C:\Users\ayaz\Downloads\untitled.fig',1)
+set(gca,'FontSize',40);
+xlabel('Harmonic Components','FontSize',40,'FontWeight','Bold')
+ylabel('Flux Density (T)','FontSize',40,'FontWeight','Bold')
+%  xlim([79000 81000]);
+%  ylim([0 0.1]);
+grid on
+L1=sprintf('Analytical Calculation ');
+L2=sprintf('FEA ');
+legend(L1,L2);
---
+++ Project-1/Result/Untitled3.m
@@ -0,0 +1,3 @@
+tet=0:0.5:360;
+T=90.54*sin(2*tet/pi);
+plot(tet,T);
